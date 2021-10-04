d63b8e7fd2a0a4b564d73e967fbce6e5e981be1a
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-10-04 17:36:52 +0200

    added reading file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io::BufRead;
+use std::fs::File;
+use std::io::BufReader;
+use std::io;
+
+enum Token {
+    Statement(String),
+    Expression(String),
+    Function(String),
+}
+
+// no idea
+fn read_file(fname: &str) -> Result<String, io::Error> {
+    let fhandle = File::open(fname)?;
+    let reader = BufReader::new(fhandle);
+    let mut out = String::new();
+
+    for (i,line) in reader.lines().enumerate() {
+        if i != 0 {
+            out.push('\n'); // no idea
+        }
+        out += &line.unwrap();
+    }
+
+    Ok(out)
+}
+
 fn main() {
-    println!("tost");
+    let source = match read_file("./tost.tst") {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("couldn't read file {}", err);
+            return;
+        }
+    };
+    println!("{}", source);
+    println!("[\x1b[0;34mtost\x1b[0m]");
 }
