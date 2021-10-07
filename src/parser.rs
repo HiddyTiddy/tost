75edbf0f88d13639e2fd1984de36c3e2ee82b19b
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-10-08 00:23:58 +0200

    parser stuff except idk what im doing

--- src/parser.rs
+++ src/parser.rs
@@ -2,8 +2,31 @@ pub mod parse_tree {
     use crate::defs::parse::Tostsken;
     use crate::defs::parse::parse_tree::*;
 
+    fn slice(tokens: Vec<Tostsken>) -> Vec<Vec<Tostsken>> {
+        // TODO: find start & end of statement
+    
+        let mut out = vec![];
 
-    fn actual_parser<'a>(tokens: Vec<Tostsken>) -> (Node<'a>, Vec<Tostsken>) {
+        let mut current = vec![];
+        for i in tokens {
+            current.push(i);
+            if true /* TODO this condition kinda is the most important part */ {
+                out.push(current);
+                current = vec![];
+            }
+        }
+
+        out
+
+    }
+
+    fn actual_parser<'a>(tokens: Vec<Tostsken>) -> Node<'a> {
+        // TODO: -> pass tokens[start..end] to actual_parser and tokens[end..] to actual_parser
+        // TODO: find start & end of tokens 
+        // TODO: fix everythinf
+
+        slice(tokens);
+
         unimplemented!();
     }
 
@@ -20,18 +43,20 @@ pub mod parse_tree {
     }
 
     impl<'a> Node<'a> {
-        pub fn new<'b>()->Node<'a> {
+        pub fn new/*<'b>*/()->Node<'a> {
             Node{children: vec![],}
         }
 
-        pub fn expand<'b>(){
+        pub fn expand/*<'b>*/(&mut self){
             
         }
     }
 
     pub fn parse<'a>(tokens: Vec<Tostsken>) -> Node<'a>{
-        let mut root = Node::new();
+        // let root = Node::new();
 
+
+        let root = actual_parser(tokens);
         // match &tokens[0] {
         //     Tostsken::OperatorOrSthIdk(op) => {
         //         if op.as_str() == "=" {
